81cce6e5025056eac0faf6727fb16b7a2c706b2e
cococrazyy <119519291+cococrazyy@users.noreply.github.com>
2026-02-25 17:37:58 -0600

    added necessary imports into app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from flask_softdelete import SoftDeleteMixin
 
 app = Flask(__name__, static_folder='.', static_url_path='')
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
 
 # Serve your existing HTML pages
 @app.route("/")
@@ -13,3 +28,4 @@ def static_files(path):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
