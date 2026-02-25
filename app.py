86945a3d03dd2f9abdf1df1d92bb68bc1003c4d0
cococrazyy <119519291+cococrazyy@users.noreply.github.com>
2026-02-25 17:53:35 -0600

    fixed classes to better represent our tables
    Added foreign keys as appropriate and created relationships

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Column, Integer, String
-from sqlalchemy import create_engine
-from flask_softdelete import SoftDeleteMixin
+from sqlalchemy import Column, Integer, String, Enum, Date, Time
+from sqlalchemy.orm import DeclarativeBase, relationship
+
 
 app = Flask(__name__, static_folder='.', static_url_path='')
 
@@ -17,6 +18,89 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 
+# creating a volunteer class
+#class Volunteer(db.Model, SoftDeleteMixin):
+class Volunteer(db.Model):
+
+    __tablename__ = "volunteers"
+
+    id = Column(Integer, primary_key=True)
+
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+
+    email = Column(String(100), unique=True)
+# creating user account class
+# only admins and captains should have be on this table
+class UserAccount(db.Model):
+    __tablename__ = "user_account"
+
+    user_id = Column(Integer, primary_key=True)
+    volunteer_id = Column(Integer, ForeignKey("volunteers.id"), unique=True)
+    password = Column(String(255), nullable=False)
+    role = Column(
+        Enum(
+            "admin",
+            "captain",
+            "volunteer",
+            "other",
+            name="role_enum"
+        ), nullable=False
+    ) 
+    volunteer = relationship("Volunteer", backref="account")
+
+# creating a stations table
+class Station(db.Model):
+    __tablename__ = "station"
+    station_id = Column(Integer, primary_key=True)
+
+    station_name = Column(
+        Enum(
+            "Setup Team",
+            "Teardown Team",
+            "Line Servers",
+            "Kitchen",
+            "Drink Station",
+            "Desserts",
+            "Busboys/sanitation",
+            "Dishwashers",
+            "Reserve",
+            "General Manager",
+            "Greeters",
+            "Baked Potato Bar",
+            "Salad Bar",
+            "Absent",
+            "Other",
+            name="station_enum"
+        )
+    )
+
+# creating schedule table
+class Schedule(db.Model):
+    __tablename__ = "schedule"
+    schedule_id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    time = Column(Time)
+
+# creating assignment table
+class Assignment(db.Model):
+    __tablename__ = "assignments"
+    assignment_id = Column(Integer, primary_key=True)
+    
+    volunteer_id = Column(Integer, ForeignKey("volunteers.id"), primary_key=True)
+    station_id = Column(Integer, ForeignKey("schedule.schedule_id"))
+    schedule_id = Column(Integer, "schedule.schedule_id")
+    
+    created_by = Column(Integer, ForeignKey("user_account.user_id"))
+
+    volunteer = relationship("Volunteer", backref = "assignments")
+    station = relationship("Station", backref = "assignments")
+    schedule = relationship("Schedule", backref = "assignments")
+    
+with app.app_context():
+    db.create_all()
+    db.session.commit()
+    
 # Serve your existing HTML pages
 @app.route("/")
 def home():
